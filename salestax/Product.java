0a369a41c2bff8731c3846ca804fc5a7bedcc964
Minhchau <holatuwol@gmail.com>
2022-03-22 10:19:22 -0700

    Move calculation to getTax() so we don't have to remember to call it first

--- salestax/Product.java
+++ salestax/Product.java
@@ -18,20 +18,6 @@ public class Product
         taxes = p_taxes;
     }
 
-    //calculates the tax for a given product p
-    public double calculateTax()
-    {
-        double total_tax = 0;
-
-        for (Tax tax : taxes) {
-            total_tax += tax.getTax(getPrice());
-        }
-
-        total_tax = Math.floor(total_tax * precision +.5) / precision;
-
-        return total_tax;
-    }
-
     //Returns the products name
     public String getName()
     {
@@ -44,16 +30,20 @@ public class Product
         return product_price;
     }
 
-    //Set the tax value for the item
-    public void setTax(double calculated_tax)
-    {
-        tax_amount = calculated_tax;
-    }
-
     //returns the total tax on the product
     public double getTax()
     {
-        return tax_amount;
+        double total_tax = 0;
+
+        for (Tax tax : taxes) {
+            total_tax += tax.getTax(getPrice());
+        }
+
+        total_tax = Math.floor(total_tax * precision +.5) / precision;
+
+        tax_amount = total_tax;
+
+        return total_tax;
     }
 
     //returns the total cost of the product including tax
